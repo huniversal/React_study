0d5f303c41905da57206058810066637d1d0a10a
huniversal <hunivers0523@naver.com>
2025-06-04 16:00:51 +0900

    feat: list components react setting

---
+++ workspace/ch02-start/vite/09/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Title from "./components/Title"
+import TodoList from "./pages/todo/List"
+
+function App() {
+  return (
+    <div id="app">
+      <div>
+        <Title />
+        <Title titleName="타이틀1" />
+        <Title titleName="타이틀2" />
+        <TodoList />
+      </div>
+    </div>
+  );
+}
+
+export default App
