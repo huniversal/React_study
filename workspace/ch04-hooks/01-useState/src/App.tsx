13fb3193bed175844c08f91d0c7b453112bdaf21
huniversal <hunivers0523@naver.com>
2025-06-13 17:38:48 +0900

    feat: useState 흐름도 주석 추가

--- workspace/ch04-hooks/01-useState/src/App.tsx
+++ workspace/ch04-hooks/01-useState/src/App.tsx
@@ -1,13 +1,21 @@
 // TODO 1. 메세지를 입력하면 입력 메세지에 반영되도록 수정
+import { useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState("");
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // e -> 이벤트 객체
+    // React.ChangeEvent<HTMLInputElement> -> 이벤트가 어떤 타입인지 설명해줌
+    setMessage(e.target.value);
+    // e.target.value -> input 안에 입력된 값을 가져오는 것
+  }
   return (
     <>
       <h1>01 useState - 상태 관리</h1>
       <div>
-        <input type="text" value="hello" />
+        <input type="text" value={message} onChange={handleChange} />
         <br />
-        <span>입력 메세지: hello</span>
+        <span>입력 메세지: {message}</span>
       </div>
     </>
   );
