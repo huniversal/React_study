3e0c2f3af8806d162415455bdcbc40c0dfd33177
huniversal <hunivers0523@naver.com>
2025-06-16 09:35:30 +0900

    feat: immer 라이브러리 사용하여 reducer 선언

--- workspace/ch12-app/todolist/03-hooks/src/pages/todoReducer.ts
+++ workspace/ch12-app/todolist/03-hooks/src/pages/todoReducer.ts
@@ -1,4 +1,5 @@
 import type {TodoItem} from "@pages/TodoItem";
+import { produce } from "immer";
 
 type TodoAction =
   | {type: 'ADD'; value: TodoItem }
@@ -9,23 +10,46 @@ type TodoAction =
 //   value: TodoItem | {_id: number};
 // }
 
-function todoReducer(state: TodoItem[], action: TodoAction):TodoItem[] {
+function todoReducer(state: TodoItem[], action: TodoAction) {
   // TODO 1. 상태 관리 로직 작성
-  switch (action.type) {
+  // switch (action.type) {
+  //   case 'ADD': 
+  //     return [...state, action.value];
+  //   case 'TOGGLE': 
+  //     return (
+  //       state.map((todo) => {
+  //         if(todo._id === (action.value._id)) {
+  //           return {...todo, done: !todo.done}
+  //         } return (todo)
+  //       })
+  //     );
+  //   case 'DELETE':
+  //     return state.filter((todo) => todo._id !== (action.value._id));
+  //   default: 
+  //     return state;
+  // }
+
+  let newState = state;
+
+  const targetIndex = state.findIndex(item => item._id === action.value._id);
+
+  switch(action.type) {
     case 'ADD': 
-      return [...state, action.value];
-    case 'TOGGLE': 
-      return (
-        state.map((todo) => {
-          if(todo._id === (action.value._id)) {
-            return {...todo, done: !todo.done}
-          } return (todo)
-        })
-      );
+      // draft : 원래 상태를 복제한 객체
+      newState = produce(state, draft => {
+        draft.push(action.value)
+      });
+      break;
+    case 'TOGGLE':
+      newState = produce(state, draft => {
+        draft[targetIndex].done = !draft[targetIndex].done; 
+      })
+      break;
     case 'DELETE':
-      return state.filter((todo) => todo._id !== (action.value._id));
-    default: 
-      return state;
+      newState = produce(state, draft => {
+        draft.splice(targetIndex, 1);
+      })
+      break;
   }
 }
 
