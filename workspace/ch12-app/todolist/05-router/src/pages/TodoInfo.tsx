81faa03ae27d4cbce69551b3fd358018d3607b9d
huniversal <hunivers0523@naver.com>
2025-06-19 10:20:03 +0900

    feat: 삭제 로직 구현 테스트

--- workspace/ch12-app/todolist/05-router/src/pages/TodoInfo.tsx
+++ workspace/ch12-app/todolist/05-router/src/pages/TodoInfo.tsx
@@ -1,4 +1,5 @@
 import { Link, Outlet, useParams, useMatch } from "react-router";
+import { useState, useEffect } from 'react';
   // useParams()는 브라우저에 명시한 URL 파라미터의 값을 가져오는 커스텀 훅
 export interface TodoItem {
   _id: number;
@@ -27,29 +28,44 @@ function TodoInfo() {
   console.log(useParams());
   const infoMatch = useMatch('/list/:_id');
 
-  // useEffect(() => {
-  //   // TODO API 서버와 통신해서 item 받아오기
+  const [data, setData] = useState<TodoItem | null>(null);
 
-  // }, [])
+  const fetchTodoInfo = () => {
+    console.log("API 서버에 상세 정보 요청");
+
+    // TODO API 서버에 상세 정보 요청
+    setData(item);
+  }
+
+  useEffect(() => {
+    fetchTodoInfo();
+
+  }, [])
 
   return (
     <div id="main">
       <h2>할일 상세 보기</h2>
-      <div className="todo">
-        <div>제목 : {item.title}</div>
-        <div>내용 : {item.content}</div>
-        <div>상태 : {item.done ? '완료' : '미완료'}</div>
-        <div>작성일 : {item.createdAt}</div>
-        <div>수정일 : {item.updatedAt}</div>
-        {/* 현재 URL이 특정 경로인지 조건부 렌더링할때 useMatch 사용 */}
-        { infoMatch && // 상세보기 화면에서만 노출
-        <>        
-          <Link to={`/list/${_id}/edit`}>수정</Link>
-          <Link to="/list">목록</Link>
-        </>}
-      </div>
 
-      <Outlet context={{ item }} />
+      { data && 
+        <>
+          <div className="todo">
+            <div>제목 : {item.title}</div>
+            <div>내용 : {item.content}</div>
+            <div>상태 : {item.done ? '완료' : '미완료'}</div>
+            <div>작성일 : {item.createdAt}</div>
+            <div>수정일 : {item.updatedAt}</div>
+            {/* 현재 URL이 특정 경로인지 조건부 렌더링할때 useMatch 사용 */}
+            { infoMatch && // 상세보기 화면에서만 노출
+              <>        
+                <Link to={`/list/${_id}/edit`}>수정</Link>
+                <Link to="/list">목록</Link>
+              </>
+            }
+            </div>
+            <Outlet context={{ item }} />
+          </>
+      }
+
       {/*  자식 라우트가 렌더링되는 위치 */}
     </div>
   );
