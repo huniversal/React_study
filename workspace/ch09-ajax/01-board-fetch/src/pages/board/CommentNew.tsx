9f248709211b4b8f81ae22cbdf3d95117af7e46e
huniversal <hunivers0523@naver.com>
2025-06-23 15:50:19 +0900

    feat: content type json으로 변경

--- workspace/ch09-ajax/01-board-fetch/src/pages/board/CommentNew.tsx
+++ workspace/ch09-ajax/01-board-fetch/src/pages/board/CommentNew.tsx
@@ -5,12 +5,27 @@ function CommentNew() {
   const [content, setContent] = useState('');
 
   // API 서버에 댓글 등록 요청
-  const requestAddComment = (formData: FormData) => {};
+  const requestAddComment = async (formData: FormData) => {
+    try {
+      await fetch( "https://fesp-api.koyeb.app/market/posts/1/replies?delay=1000", {
+        headers: {
+          'Client-ID': 'openmarket',
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+        body: formData
+      });
+    } catch(err) {
+      console.error(err);
+    } 
+  };
 
   const handleAddComment = ((e: React.FormEvent) => {
     e.preventDefault();
 
     const formData = new FormData();
+    console.log('content', content);
+    formData.append('content', content);
     requestAddComment(formData);
     
 
