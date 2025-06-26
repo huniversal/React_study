5ebe8a3f73a371802c6f9ca2944cfa90d3d0d3ab
huniversal <hunivers0523@naver.com>
2025-06-26 13:16:31 +0900

    feat: zustand 테스트

--- workspace/ch07-globalstate/03-zustand/src/components/Right3.tsx
+++ workspace/ch07-globalstate/03-zustand/src/components/Right3.tsx
@@ -1,13 +1,17 @@
+import useCounterStore from '@/zustand/counter';
 import { useEffect } from 'react';
 
 function Right3() {
   useEffect(()=>{
     console.log('#### Right3 렌더링.');
   });
+  const { countUp, countDown, countReset } = useCounterStore();  
   return (
     <div>
       <h3>Right3</h3>
-      <button onClick={ () => {} }>+1</button>
+      <button onClick={ () => countUp(1) }>+1</button>
+      <button onClick={ () => countReset() }>0</button>
+      <button onClick={ () => countDown(1) }>-1</button>
     </div>
   );
 }
