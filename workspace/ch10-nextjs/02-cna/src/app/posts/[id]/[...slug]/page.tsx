b31907ff835ca3aaba7ae172888c6d444445ce94
huniversal <hunivers0523@naver.com>
2025-07-01 11:39:06 +0900

    feat: 게시글 id 추가

--- workspace/ch10-nextjs/02-cna/src/app/posts/[id]/[...slug]/page.tsx
+++ workspace/ch10-nextjs/02-cna/src/app/posts/[id]/[...slug]/page.tsx
@@ -10,7 +10,7 @@ export default async function SlugPage({params} : {params: {id: string, slug: st
   }
   return (
     <>
-      <h1>{ slugParams.slug[0] === 'like' ? '좋아요 목록' : '즐겨찾기 목록' }</h1>
+      <h1>{ slugParams.id}번 게시물의 { slugParams.slug[0] === 'like' ? '좋아요 목록' : '즐겨찾기 목록' }</h1>
       { slugParams.slug[1] && <h2>{slugParams.slug[1] } 상세 정보 출력</h2> }
     </>
   );
