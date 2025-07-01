00196c809c89712ca9a53b13387f7617236fd2d3
huniversal <hunivers0523@naver.com>
2025-07-01 11:31:43 +0900

    feat: Catch-all 세그먼트를 사용해 좋아요, 즐겨찾기 목록 출력

---
+++ workspace/ch10-nextjs/02-cna/src/app/posts/[id]/[...slug]/page.tsx
@@ -0,0 +1,14 @@
+export default async function SlugPage({params} : {params: {id: string, slug: string[] } } ) {
+  const slugParams = await params;
+  console.log(slugParams);
+  switch (slugParams.slug[0]) {
+    case 'like':
+      // 좋아요 목록 출력
+    case 'favorite':
+      // 츨겨찾기 목록 출력
+    
+  }
+  return (
+    <h1>{ slugParams.slug[0] === 'like' ? '좋아요 목록' : '즐겨찾기 목록' }</h1>
+  );
+}
