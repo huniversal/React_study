aee957260f3f7a24fa4828abffaa52a64adb6b48
huniversal <hunivers0523@naver.com>
2025-07-02 11:40:44 +0900

    feat: NextRequest, NextResponse 테스트

--- workspace/ch10-nextjs/02-cna/src/app/api/posts/[id]/route.ts
+++ workspace/ch10-nextjs/02-cna/src/app/api/posts/[id]/route.ts
@@ -1,12 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server';
+
 // 지원 메소드 선언
-export default function GET() {
+export async function GET(request: NextRequest, {params}: {params: Promise<{id: string}>}) {
+  const { id } = await params;
+  console.log("GET 라우트 핸들러", id);
+  // 실제 작업
+
+
+  const data = {id, title: "가짜 제목"}
+  return NextResponse.json(data);
+}
+
+export function POST() {
+  return NextResponse.json("route handler의 POST 응답");
   
 }
 
-export default function POST() {
-  
-}
-
-export default function DELETE() {
+export function DELETE() {
   
 }
