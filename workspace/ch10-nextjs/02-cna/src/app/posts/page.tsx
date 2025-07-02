d4fc8452cc53454db11052addea6495203eb7bf9
huniversal <hunivers0523@naver.com>
2025-07-02 14:25:30 +0900

    feat: 서버에서 게시글 데이터 목록 조회 기능 로직 구현

--- workspace/ch10-nextjs/02-cna/src/app/posts/page.tsx
+++ workspace/ch10-nextjs/02-cna/src/app/posts/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import Link from "next/link";
+import { fetchPosts } from "@/data/functinos/boardFetch";
 
 export const metadata: Metadata = {
   title: "게시물 목록 조회",
@@ -7,19 +8,22 @@ export const metadata: Metadata = {
 };
 export default async function ListPage() {
   await new Promise((res) => {setTimeout(res, 2000)});
-  const dummyList = [];
-  for (let i = 1; i <= 10; i++) {
-    dummyList.push({
-      id: i,
-      title: i + "번 게시물",
-    });
-  }
+  // const dummyList = [];
+  // for (let i = 1; i <= 10; i++) {
+  //   dummyList.push({
+  //     id: i,
+  //     title: i + "번 게시물",
+  //   });
+  // }
 
-  const posts = dummyList.map((post) => (
-    <li key={post.id}>
+  const data = await fetchPosts();
+
+  console.log("API 서버로부터 받은 게시물 목록 수", data.length);
+  const posts = data.map((post) => (
+    <li key={post._id}>
       {/* <Link prefetch={true} href={`/posts/${post.id}`}> */}
-      <Link href={`/posts/${post.id}`}>
-        {post.title}
+      <Link href={`/posts/${post._id}`}>
+        {post._id} - {post.title}
       </Link>
     </li>
   ));
