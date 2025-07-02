135f9a26ef240a4c9398824741078511363436d8
huniversal <hunivers0523@naver.com>
2025-07-02 15:35:17 +0900

    feat: CSR 및 SSR 방식으로 GET 테스트

--- workspace/ch10-nextjs/02-cna/src/app/posts/page.tsx
+++ workspace/ch10-nextjs/02-cna/src/app/posts/page.tsx
@@ -1,27 +1,67 @@
-import { Metadata } from "next";
+// import { Metadata } from "next";
+// import Link from "next/link";
+// import { fetchPosts } from "@/data/functinos/boardFetch";
+
+// export const metadata: Metadata = {
+//   title: "게시물 목록 조회",
+//   description: "게시물 목록 조회 페이지입니다.",
+// };
+// export default async function ListPage() {
+//   await new Promise((res) => {setTimeout(res, 2000)});
+//   // const dummyList = [];
+//   // for (let i = 1; i <= 10; i++) {
+//   //   dummyList.push({
+//   //     id: i,
+//   //     title: i + "번 게시물",
+//   //   });
+//   // }
+
+//   const data = await fetchPosts();
+
+//   console.log("API 서버로부터 받은 게시물 목록 수", data.length);
+//   const posts = data.map((post) => (
+//     <li key={post._id}>
+//       {/* <Link prefetch={true} href={`/posts/${post.id}`}> */}
+//       <Link href={`/posts/${post._id}`}>
+//         {post._id} - {post.title}
+//       </Link>
+//     </li>
+//   ));
+
+//   return (
+//     <>
+//       <h1>목록 조회</h1>
+//       <ul>{posts}</ul>
+//     </>
+//   );
+// }
+
+"use client"
+// import { Metadata } from "next";
 import Link from "next/link";
 import { fetchPosts } from "@/data/functinos/boardFetch";
+import { Post } from "@/types/board";
+import { useEffect, useState } from 'react';
 
-export const metadata: Metadata = {
-  title: "게시물 목록 조회",
-  description: "게시물 목록 조회 페이지입니다.",
-};
-export default async function ListPage() {
-  await new Promise((res) => {setTimeout(res, 2000)});
-  // const dummyList = [];
-  // for (let i = 1; i <= 10; i++) {
-  //   dummyList.push({
-  //     id: i,
-  //     title: i + "번 게시물",
-  //   });
-  // }
+// export const metadata: Metadata = {
+//   title: "게시물 목록 조회",
+//   description: "게시물 목록 조회 페이지입니다.",
+// };
+export default function ListPage() {
+  const [ data, setData ] = useState<Post[] | null>(null);
 
-  const data = await fetchPosts();
+  async function fetchAsyncPosts() {
+    const resData = await fetchPosts();
+    setData(resData);
+  }
 
-  console.log("API 서버로부터 받은 게시물 목록 수", data.length);
-  const posts = data.map((post) => (
+  useEffect(() => {
+    fetchAsyncPosts();
+  }, []) // 마운트 때 한번만 호출
+
+  console.log("API 서버로부터 받은 게시물 목록 수", data?.length);
+  const posts = data?.map((post) => (
     <li key={post._id}>
-      {/* <Link prefetch={true} href={`/posts/${post.id}`}> */}
       <Link href={`/posts/${post._id}`}>
         {post._id} - {post.title}
       </Link>
--- workspace/ch10-nextjs/02-cna/src/data/functinos/boardFetch.ts
+++ workspace/ch10-nextjs/02-cna/src/data/functinos/boardFetch.ts
@@ -1,3 +1,5 @@
+"use server";
+
 import { Post } from "@/types/board";
 
 export async function fetchPosts (): Promise<Post[]> {
@@ -9,5 +11,6 @@ export async function fetchPosts (): Promise<Post[]> {
   });
 
   const data = await res.json();
+  console.log('boardFetch', data.item.length);
   return data.item;
 }
