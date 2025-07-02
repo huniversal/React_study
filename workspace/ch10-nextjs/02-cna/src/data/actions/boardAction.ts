43ac083cdae6ce841e047281f2ff915a98265c11
huniversal <hunivers0523@naver.com>
2025-07-03 00:13:43 +0900

    feat: 게시글 작성 서버 POST 요청 테스트

--- workspace/ch10-nextjs/02-cna/src/data/actions/boardAction.ts
+++ workspace/ch10-nextjs/02-cna/src/data/actions/boardAction.ts
@@ -1,15 +1,20 @@
-"use server"; // 서버 액션
+"use server";
+import { PostInfoRes } from '@/types/board';
+
+ // 서버 액션
 
 // 게시글 등록
-export async function createPost(formData: FormData) {
+export async function createPost(prevState: PostInfoRes, formData: FormData) {
   const title = formData.get('title');
   const content = formData.get('content');
-
-  const res = await fetch(`https://fesp-api.koyeb.app/market/posts?type=qna`, {
+  const body = {title, content, type: 'qna'};
+  console.log(body);
+  const res = await fetch(`https://fesp-api.koyeb.app/market/posts`, {
     method: 'POST',
-    body: JSON.stringify({title, content}),
+    body: JSON.stringify({title, content, type: 'qna'}),
     headers: {
-      'Client-Id': 'openmarket'
+      'Client-Id': 'openmarket',
+      'Content-Type': 'application/json',
     }
   });
   const data = await res.json();
