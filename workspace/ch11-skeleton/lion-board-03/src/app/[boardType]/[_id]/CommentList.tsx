114c25ffa98afe4b7eb233215ed0a9511b99ae56
huniversal <hunivers0523@naver.com>
2025-07-06 00:46:00 +0900

    feat: 댓글 목록 조회 서버 함수 호출

--- workspace/ch11-skeleton/lion-board-03/src/app/[boardType]/[_id]/CommentList.tsx
+++ workspace/ch11-skeleton/lion-board-03/src/app/[boardType]/[_id]/CommentList.tsx
@@ -2,13 +2,20 @@ import CommentNew from "@/app/[boardType]/[_id]/CommentNew"
 import CommentItem from "@/app/[boardType]/[_id]/CommentItem";
 import Image from "next/image";
 import Link from "next/link";
+import { getReplies } from "@/data/functions/post";
+import { PostReply } from "@/types";
 
-export default function CommentList() {
+export default async function CommentList({_id}: {_id: string}) {
+  const res = await getReplies(Number(_id));
   return (
     <section className="mb-8">
-      <h4 className="mt-8 mb-4 ml-2">댓글 2개</h4>
-      <CommentItem />
-      <CommentItem />
+      <h4 className="mt-8 mb-4 ml-2">댓글 { res.ok ? res.item.length : 0 }개</h4>
+        { res.ok ? 
+          res.item.map((reply: PostReply) => (
+            <CommentItem key={reply._id} reply={reply} />
+          )) : 
+          <p>{ res.message }</p>
+        }
       <div className="shadow-md rounded-lg p-4 mb-4">
         <div className="flex justify-between items-center mb-2">
           <div className="flex items-center">
@@ -30,7 +37,7 @@ export default function CommentList() {
           </form>
         </div>
       </div>
-      <CommentNew />
+      <CommentNew _id={ _id } />
     </section>
   )
 }
